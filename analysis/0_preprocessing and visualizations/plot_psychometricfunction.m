31ee5467e716c909b422adb1285cdffa1d8326dc
aspenyoo <aspen.yoo@nyu.edu>
2016-05-25 00:00:40 -0400

    ahy analysis

--- analysis/0_preprocessing and visualizations/plot_psychometricfunction.m
+++ analysis/0_preprocessing and visualizations/plot_psychometricfunction.m
@@ -1,10 +1,10 @@
 % plot psychometric function for single or multiple subjects
 
-subjids = {'PM','SM','DG','SC'};
-condidx = [4 1 2 3];
+subjids = {'AHY'};
+condidx = [4 1];
 
 delta = 5:5:90;
-nCond = 4;%length(data);
+nCond = length(condidx);
 nSubj = length(subjids);
 
 figure;
@@ -15,7 +15,7 @@ for isubj = 1:nSubj;
     
     % load file
     subjid = subjids{isubj};
-    load(sprintf('data_ChangeLocalization_subj%s_04282016.mat',subjid));
+    load(sprintf('data_ChangeLocalization_subj%s_05242016.mat',subjid));
     
     for icond = 1:nCond;
         
@@ -55,8 +55,8 @@ delta = deltanew;
 subplot(2,2,1);
 hold on;
 errorbar(delta,squeeze(mean(pc(:,1,:),1)),squeeze(std(pc(:,1,:),[],1))/sqrt(nSubj),'Color','k','LineStyle','-');
-errorbar(delta,squeeze(mean(pc(:,4,:),1)),squeeze(std(pc(:,4,:),[],1))/sqrt(nSubj),'Color',0.7*ones(1,3),'LineStyle','-');
-legend('4/1','0/2')
+errorbar(delta,squeeze(mean(pc(:,2,:),1)),squeeze(std(pc(:,2,:),[],1))/sqrt(nSubj),'Color',0.7*ones(1,3),'LineStyle','-');
+legend('2nd interval','1st interval')
 defaultplot;
 title('pc for 0 and 4 condition')
 
