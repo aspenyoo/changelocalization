6185f3833712107f46d3b2573c4f77c50a7c145b
Aspen H. Yoo <aspen.yoo@nyu.edu>
2017-04-29 12:26:53 -0400

    debug

--- analysis/2_fitdata/fit_grant_jun2016.m
+++ analysis/2_fitdata/fit_grant_jun2016.m
@@ -2,8 +2,8 @@ function fit_grant_jun2016(subjid,model,runlist,runmax,nSamples)
 %GRANT_FIT_JUN2016 Fit two subjects for the grant
 
 % Optimization restarts
-if nargin < 2 || isempty(runmax); runmax = 50; end
-if nargin < 3; nSamples = []; end   % Use default
+if nargin < 4 || isempty(runmax); runmax = 50; end
+if nargin < 5; nSamples = []; end   % Use default
 
 fit_maximum_likelihood(subjid,model,'Contrast',runlist,runmax,nSamples);
 fit_maximum_likelihood(subjid,model,'Delay',runlist,runmax,nSamples);
